03928197caaa1865449e3684e0bf1c2447e55c73
Eranachandran <eranachandran@corestack.io>
2020-08-29 19:09:46 +0530

    test clone

--- trigger/Jenkinsfile.groovy
+++ trigger/Jenkinsfile.groovy
@@ -3,10 +3,10 @@ pipeline {
 		label 'master'
 	}
 	parameters {
-    	string defaultValue: 'v2.9.5_FR', 
+    	string (defaultValue: 'v2.9.5_FR', 
     	description: 'branch name of the repository', 
     	name: 'branch', 
-    	trim: false
+    	trim: false)
     }
 
 	stages {
@@ -14,14 +14,15 @@ pipeline {
 			steps {
 				checkout([
 					$class: 'GitSCM', 
-					branches: [[name: '*/params.branch']],
+					branches: [[name: '*/${params.branch}']],
 					doGenerateSubmoduleConfigurations: false,
-					extensions: [[$class: 'LocalBranch', localBranch: 'params.branch'], 
+					extensions: [[$class: 'LocalBranch', localBranch: '${params.branch}'], 
 					[$class: 'CleanBeforeCheckout'], 
 					[$class: 'RelativeTargetDirectory', relativeTargetDir: 'heatstack']],
 					submoduleCfg: [], 
 					userRemoteConfigs: [[credentialsId: '2e5f717b-82f6-4526-a358-42e77b16c6d5',url: 'https://review.corestack.in/heatstack']]
 					])
+				echo ${params.region}
 			}
 		}
 	}
