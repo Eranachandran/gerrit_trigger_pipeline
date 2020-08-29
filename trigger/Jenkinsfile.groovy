89bc8132f706309e0890414d660d1b4d46a9e437
Eranachandran <eranachandran@corestack.io>
2020-08-29 19:15:02 +0530

    test clone

--- trigger/Jenkinsfile.groovy
+++ trigger/Jenkinsfile.groovy
@@ -12,16 +12,6 @@ pipeline {
 	stages {
 		stage('clone_pipeline-test') {
 			steps {
-				checkout([
-					$class: 'GitSCM', 
-					branches: [[name: '*/${params.branch}']],
-					doGenerateSubmoduleConfigurations: false,
-					extensions: [[$class: 'LocalBranch', localBranch: '${params.branch}'], 
-					[$class: 'CleanBeforeCheckout'], 
-					[$class: 'RelativeTargetDirectory', relativeTargetDir: 'heatstack']],
-					submoduleCfg: [], 
-					userRemoteConfigs: [[credentialsId: '2e5f717b-82f6-4526-a358-42e77b16c6d5',url: 'https://review.corestack.in/heatstack']]
-					])
 				echo "${params.branch}"
 			}
 		}
